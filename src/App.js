9ff1da710a3c57cc8873e855a2481e86471ec7e9
mohammad-najem <mohammad-am@live.com>
2021-05-18 17:03:47 +0300

    add redux products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // feature 1
 import React from 'react';
+import { Provider } from 'react-redux';
 import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from "./data.json";
+import store from "./store";
 
 class App extends React.Component {
   constructor(){
@@ -76,6 +78,7 @@ class App extends React.Component {
   };
   render() {
   return (
+    <Provider store={store}>
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
@@ -106,6 +109,7 @@ class App extends React.Component {
         All right is reserved.
       </footer>
     </div>
+    </Provider>
   );
   }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,13 +3,18 @@ import formatCurrency from '../util';
 import Fade from 'react-reveal/Fade';
 import Modal from 'react-modal';
 import Zoom from 'react-reveal/Zoom';
+import { connect } from "react-redux";
+import { fetchProducts } from "../actions/productActions";
 
-export default class Products extends Component {
+class Products extends Component {
     constructor(props){
         super(props);
         this.state = {
             product: null,
         };
+    }
+    componentDidMount() {
+        this.props.fetchProducts();
     }
     openModal = (product) => {
         this.setState({ product : null });
@@ -18,27 +23,36 @@ export default class Products extends Component {
         const {product} = this.state;
         return (
             <div>
-                <Fade bottom cascade>
-                <ul className="products">
-                    {this.props.products.map(product =>(
-                        <li key={product._id}>
-                            <div className="product">
-                                <a href={"#" +  product._id} onClick={() => this.openModal(product)}>
-                                    <img src={product.image} alt={product.title}></img>
-                                    <p>
-                                        {product.title}
-                                    </p>
-                                </a>
-                                <div className="product-price">
-                                    <div>{formatCurrency(product.price)}</div>
-                                    <button onClick={(e)=> this.props.addToCart(product)} className="button primary">Add To Cart</button>
-                                </div>
-                            </div>
-                        </li>
-                    ))
-                    }
-                </ul>
-                </Fade>
+        <Fade bottom cascade>
+          {!this.props.products ? (
+            <div>Loading...</div>
+          ) : (
+            <ul className="products">
+              {this.props.products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
+                    <a
+                      href={"#" + product._id}
+                      onClick={() => this.openModal(product)}
+                    >
+                      <img src={product.image} alt={product.title}></img>
+                      <p>{product.title}</p>
+                    </a>
+                    <div className="product-price">
+                      <div>{formatCurrency(product.price)}</div>
+                      <button
+                        onClick={() => this.props.addToCart(product)}
+                        className="button primary"
+                      >
+                        Add To Cart
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </Fade>
                 {product && (
                     <Modal isOpen={true} onRequestClose={this.closeModal}>
                         <Zoom>
@@ -81,3 +95,9 @@ export default class Products extends Component {
         );
     }
 }
+export default connect(
+    (state) => ({ products: state.products.items }), 
+    {
+        fetchProducts
+    }
+    )(Products);
