afc32d6b48d6ccf7081c3155fb227ab6ecc66719
mohammad-najem <mohammad-am@live.com>
2021-05-18 18:10:36 +0300

    add redux to Filter component

--- src/components/Products.js
+++ src/components/Products.js
@@ -96,7 +96,7 @@ class Products extends Component {
     }
 }
 export default connect(
-    (state) => ({ products: state.products.items }), 
+    (state) => ({ products: state.products.filteredItems }), 
     {
         fetchProducts
     }
